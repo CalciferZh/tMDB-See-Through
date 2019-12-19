60ee8ad232b64cebaf8fa85d074d6ee52bf7c95e
Vica Yang <22126501+VicaYang@users.noreply.github.com>
2019-12-19 14:19:19 +0800

    show label for highlighted element

--- static/js/Layout.js
+++ static/js/Layout.js
@@ -263,7 +263,7 @@ LayoutClass = function() {
     d3.selectAll(".visiable-name")
       .attr("x", d => d.cx + d.r)
       .attr("y", d => d.cy + d.r / 2)
-      .text(d => (d.r > 8 ? d.info.abbr : ""))
+      .text(d => (d.r > 15 || d.highlighted ? d.info.abbr : ""))
       .attr("opacity", d =>
       1.5 * (d.highlighted ? that.highlight_point_opacity : d.opacity));
 
